307b7b98d382c81102d2a92d05b8c0273ed5ff1c
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-02-15 17:02:43 -0700

    Update hw06a.py

--- hw6/hw06a.py
+++ hw6/hw06a.py
@@ -1,57 +1,68 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 15 08:25:17 2023
+import math
+import argparse
+import sys
 
-@author: halle
-"""
-from numpy import np
-from scipy.optimize import brentq
-
-# Constants for Si
-B = 1.08e-16 # cm^3/K^3/2
-Eg = 1.12 # eV
-k = 8.617e-5 # eV/K
-
-# Constants for Ge
-B_Ge = 7.6e18 # cm^-3 K^-3/2
-Eg_Ge = 0.66 # eV
-k_Ge = 8.617e-5 # eV/K
-
-# Constants for GeAs
-B_GeAs = 8.4e18 # cm^-3 K^-3/2
-Eg_GeAs = 1.52 # eV
-k_GeAs = 8.617e-5 # eV/K
-
-def FindT(ni, a, b, element):
+def FindT(ni, a=1, b=1000, element="Si"):
     if element == "Si":
-        B_ = B
-        Eg_ = Eg
-        k_ = k
+        B = 3.6e16
+        EG = 1.12
     elif element == "Ge":
-        B_ = B_Ge
-        Eg_ = Eg_Ge
-        k_ = k_Ge
+        B = 1.5e19
+        EG = 0.67
     elif element == "GeAs":
-        B_ = B_GeAs
-        Eg_ = Eg_GeAs
-        k_ = k_GeAs
+        B = 5.5e19
+        EG = 0.82
     else:
-        raise ValueError("Invalid element")
-
+        raise ValueError("Invalid element: " + element)
+    k = 8.617333262e-5 # Boltzmann constant in eV/K
     def f(T):
-        return ni**2-B_*(T**3)* np.exp(-Eg_ / (2 * k_ * T))
+        return ni**2 - B * T**3 * math.exp(-EG/(k*T))
+    if f(min) * f(a) >= 0:
+        raise ValueError("No solution in the given range")
 
+# =============================================================================
+# def main():
+#     parser = argparse.ArgumentParser()
+#     parser.add_argument("ni", type=float)
+#     parser.add_argument("min", type=float, nargs="?", default=1)
+#     parser.add_argument("max", type=float, nargs="?", default=1000)
+#     parser.add_argument("element", type=str, nargs="?", default="Si")
+#     args = parser.parse_args()
+#     try:
+#         T = FindT(args.ni, args.a, args.b, args.element)
+#         print("T={:.2e}".format(T))
+#     except ValueError as e:
+#         print("No solution:", e)
+#         return -1
+# 
+# =============================================================================
+
+#also try this #===========================================================================
+def main(args):
+    if len(args) < 2:
+        print("Usage: {} ni [min] [max] [element]".format(args[0]))
+        return -1
+
+    ni = float(args[1])
+    min_val = float(args[2]) if len(args) > 2 else 1
+    max_val = float(args[3]) if len(args) > 3 else 1000
+    element = args[4] if len(args) > 4 else "Si"
+    print (args[1])
+    print (args[2])
+    print (args[3])
+    print (args[4])
+    
     try:
-        T = brentq(f, a, b)
-    except ValueError:
-        raise ValueError("No solution found")
+        T = FindT(ni, min_val, max_val, element)
+        print("T={:.2e}".format(T))
+    except ValueError as e:
+        print("No solution ", e)
 
-    return T
-    
-def main():
-    #put input from sys argv locations 0,1,2 into function of 
-    
-    
-if __name__ =="__main__":
-    main()
+
+
+# if __name__ == "__main__":
+#     sys.exit(main(sys.argv))
+if __name__ == "__main__":
+     main()
+
 
