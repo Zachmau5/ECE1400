63bc3854687044e4fa87f18662bcb39c9db046c8
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-03-12 16:34:55 -0600

    Delete hw6 directory

--- hw6/hw06c.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# read Spice output file into a pandas dataframe
-data = pd.read_csv("../test/data.correct")
-data.plot(x="time",ylabel="current (mA)",xlabel="time (sec)",title="Spice Output")
-plt.show()
