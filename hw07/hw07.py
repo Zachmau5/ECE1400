85dbc25b36ff13b3f13eeb464deb320a7bd2c95f
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-03-12 18:59:45 -0600

    Update hw07.py

--- hw07/hw07.py
+++ hw07/hw07.py
@@ -6,29 +6,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.integrate as inte
+
 def data(cal, imu, out):
     f=open(cal)
     cal=json.load(f)
     f.close()
-
+    
+    #using pandas to create an empty array at 'loc,col,value'
+    
     df=pd.read_csv("../test/{}.csv".format(imu))
     df.insert(2, "v_x", np.zeros(4000))
     df.insert(3, "v_y", np.zeros(4000))
     df.insert(4, "v_z", np.zeros(4000))
-
+    
+    #filling arrays with integrated values to get mps and rps
     df['v_x'][1:]=inte.cumtrapz((df['a_x']*cal['a_x_scale']), df['time'], dx=1.0, axis=-1, initial=None)
     df['v_y'][1:]=inte.cumtrapz((df['a_y']*cal['a_y_scale']), df['time'], dx=1.0, axis=-1, initial=None)
     df['v_z'][1:]=inte.cumtrapz((df['a_z']*cal['a_z_scale']), df['time'], dx=1.0, axis=-1, initial=None)
 
-
+    #same as before, creating empty arrays
     df.insert(5, "x", np.zeros(4000))
     df.insert(6, "y", np.zeros(4000))
     df.insert(7, "z", np.zeros(4000))
-
+    
+    #integrate again for y values of mps andrps
     df['x'][1:]= inte.cumtrapz(df['v_x'], df['time'], dx=1.0, axis=-1, initial=None)
     df['y'][1:]= inte.cumtrapz(df['v_y'], df['time'], dx=1.0, axis=-1, initial=None)
     df['z'][1:]= inte.cumtrapz(df['v_z'], df['time'], dx=1.0, axis=-1, initial=None)
-
+    
+    #plotting data
     fig, axs = plt.subplots(3)
     fig.set_figheight(9)
     fig.set_figwidth(8)
@@ -41,12 +47,15 @@ def data(cal, imu, out):
     axs[2].set_ylabel("Accel [m/s^2]")
     df.plot(x='time', y=['a_x','a_y','a_z'], ax=axs[2])
 
+    #save fig
     plt.savefig('{}_linear.png'.format(imu))
-
+    
+    #calculating roll,pitch,yaw and creating empty array
     df.insert(11, "roll", np.zeros(4000))
     df.insert(12, "pitch", np.zeros(4000))
     df.insert(13, "yaw", np.zeros(4000))
-
+    
+    #mps and rps
     df['roll'][1:]=inte.cumtrapz((df['v_roll']*cal['v_roll_scale']), df['time'], dx=1.0, axis=-1, initial=None)
     df['pitch'][1:]=inte.cumtrapz((df['v_pitch']*cal['v_pitch_scale']), df['time'], dx=1.0, axis=-1, initial=None)
     df['yaw'][1:]=inte.cumtrapz((df['v_yaw']*cal['v_yaw_scale']), df['time'], dx=1.0, axis=-1, initial=None)
@@ -54,11 +63,13 @@ def data(cal, imu, out):
     df.insert(17, "a_roll", np.zeros(4000))
     df.insert(18, "a_pitch", np.zeros(4000))
     df.insert(19, "a_yaw", np.zeros(4000))
-
+    
+    #partial vroll/partial time
     df['a_roll']=df['v_roll'].diff()/df['time'].diff()
     df['a_pitch']=df['v_pitch'].diff()/df['time'].diff()
     df['a_yaw']=df['v_yaw'].diff()/df['time'].diff()
 
+    #plotting and saving fig
     fig, axs = plt.subplots(3)
     fig.set_figheight(9)
     fig.set_figwidth(8)
@@ -76,6 +87,8 @@ def data(cal, imu, out):
 
 
 def main():
+    #makes sure length is long enough
+    
     l=len(sys.argv)
     if l == 4:
         cal=sys.argv[1]
