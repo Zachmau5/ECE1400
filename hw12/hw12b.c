8f85611f69397ccd3cfec5a8f172b94238f11e60
Zachmau5 <123523690+Zachmau5@users.noreply.github.com>
2023-04-16 12:54:11 -0600

    Update hw12b.c

--- hw12/hw12b.c
+++ hw12/hw12b.c
@@ -15,20 +15,26 @@ int main(int argc, char *argv[]){
     struct Person head;
     head.prev=NULL;
     head.next=NULL;
-
+    
     FILE *in_file=fopen(filein,"r");
-        if(in_file==NULL){
-        printf("no input found ya dumb dumb");
-            return 1;
-        }
-        char son[20];
-        char father[50];
-        char mother[50];
-        int gen=0;
-        while(fscanf(in_file,"%s%s%s",son,father,mother) ==3) {
-    addToTail(&head,son,father,mother);
-    }
-    printList(&head);
+    	if(in_file==NULL){
+    	printf("no input found ya dumb dumb");
+    		return 1;
+    		}
+    	char child[20];
+    	char father[50];
+    	char mother[50];
+    	int gen=0;
+    	while(fscanf(in_file,"%s%s%s",child,father,mother) ==3) {
+		addToTail(&head,child,father,mother);
+		}
+//Finds and prints ancestors
+    printf("ancestors of %s:\n",name);
     findAncestor(&head,name,gen);
-
+//Finds and Prints decendants
+    printf("descendants of %s:\n",name);
+    findDecendant(&head,name,gen);
+//XTRA, finds siblings. Removed for test
+    //printf("siblings of %s:\n",name);
+    //printSiblings(&head, name);
 }
